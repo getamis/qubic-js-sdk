ecdf52ad007644075c9dc814c6bb970f3d13a9f0
Steven Fong <waneblade@gmail.com>
2021-07-03 00:25:13 +0800

    show button before open window

--- packages/browser/src/ui/PopupWindow.ts
+++ packages/browser/src/ui/PopupWindow.ts
@@ -13,18 +13,55 @@ window.qubic = window.qubic || {
   },
 };
 
-class IFrame {
+class OpenPopupWindow {
   qubicWindow: Window | null;
   url = '';
+  element: HTMLDivElement;
   constructor(url: string) {
+    const div = document.createElement('div');
+    div.style.display = 'none';
+    div.style.position = 'fixed';
+    div.style.top = '0';
+    div.style.left = '0';
+    div.style.right = '0';
+    div.style.bottom = '0';
+    div.style.zIndex = '99999';
+    div.style.backgroundColor = '#00000066';
+    div.style.borderWidth = '0';
+    div.style.width = '100%';
+    div.style.height = '100%';
+    div.style.background = 'rgba(0,0,0,.5)';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'center';
+    const button = document.createElement('button');
+    button.innerHTML = 'Continue Qubic';
+    button.style.paddingRight = '8px';
+    button.style.paddingLeft = '8px';
+    button.style.paddingTop = '4px';
+    button.style.paddingBottom = '4px';
+    div.appendChild(button);
+    this.element = div;
+    document.body.appendChild(this.element);
+    button.onclick = () => {
+      this.hide();
+      this.openPopupWindow();
+      const task = this.queue.shift();
+      if (task) {
+        const { action, payload } = task;
+        setTimeout(() => {
+          // this.qubicWindow.onload = xxx
+          // this.qubicWindow.addEventListener('load', cb, false)
+          // this.qubicWindow.addEventListener('onload', cb, false)
+          // all is not firing, maybe we can use postMessage from popup to inform this window
+          this.qubicWindow?.postMessage({ action, payload }, '*');
+        }, 2000);
+      }
+    };
+
     this.url = url;
     this.qubicWindow = null;
     window.qubic.show = () => {
-      if (!this.qubicWindow || this.qubicWindow.closed) {
-        const target = 'qubic-wallet';
-        const windowFeatures = 'location=no,resizable=yes,scrollbars=yes,status=yes,height=600,width=500';
-        this.qubicWindow = window.open(this.url, target, windowFeatures);
-      }
+      this.element.style.display = 'flex';
     };
 
     window.addEventListener(
@@ -41,21 +78,30 @@ class IFrame {
     );
   }
 
+  public openPopupWindow = (): void => {
+    if (!this.qubicWindow || this.qubicWindow.closed) {
+      const target = 'qubic-wallet';
+      const windowFeatures = 'location=no,resizable=yes,scrollbars=yes,status=yes,height=600,width=500';
+      this.qubicWindow = window.open(this.url, target, windowFeatures);
+    }
+  };
+
+  public show = (): void => {
+    this.element.style.display = 'flex';
+  };
+
   public hide = (): void => {
+    this.element.style.display = 'none';
     this.qubicWindow?.postMessage({ action: 'hideIframeDone' }, '*');
     this.qubicWindow?.close();
     this.qubicWindow = null;
   };
 
-  public open = (action: string, payload?: { [key: string]: any }): void => {
-    setTimeout(() => {
-      // this.qubicWindow.onload = xxx
-      // this.qubicWindow.addEventListener('load', cb, false)
-      // this.qubicWindow.addEventListener('onload', cb, false)
-      // all is not firing, maybe we can use postMessage from popup to inform this window
-      this.qubicWindow?.postMessage({ action, payload }, '*');
-    }, 2000);
+  public queue: Array<{ action: string; payload?: Record<string, any> }> = [];
+
+  public open = (action: string, payload?: Record<string, any>): void => {
+    this.queue.push({ action, payload });
   };
 }
 
-export default IFrame;
+export default OpenPopupWindow;
