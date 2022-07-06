1a6aef9763060d3ae7bf04ab4aa43295ba974af3
Steven Fong <waneblade@gmail.com>
2022-07-06 21:47:06 +0800

    [ASL-2691] update in app hint message

--- packages/browser/src/translation.ts
+++ packages/browser/src/translation.ts
@@ -7,8 +7,9 @@ const enUs = {
   yes: 'Yes',
   no: 'No',
   'copy-link': 'Copy Link',
-  'copy-failed': 'Copy failed. Please copy the link manually',
-  'in-app-hint': 'Please open this page in Chrome',
+  'copy-failed-android': 'Copy failed. Please copy the link manually and paste in Chrome',
+  'copy-failed-ios': 'Copy failed. Please copy the link manually and paste in Safari',
+  'in-app-hint-android': 'Please open this page in Chrome',
   'in-app-hint-ios': 'Please open this page in Safari',
   'popup-window-hint': 'Attempt to open Qubic to complete the action. Do you want to proceed ?',
 };
@@ -19,9 +20,10 @@ const zhTw: Record<localeKeys, string> = {
   yes: '是',
   no: '否',
   'copy-link': '複製連結',
-  'copy-failed': '複製失敗。請手動複製網址',
-  'in-app-hint': '請在 Chrome 中開啟此頁',
-  'in-app-hint-ios': '請在 Safari 中開啟此頁',
+  'copy-failed-android': '複製失敗。請手動複製網址並在 Chrome 中開啟頁面',
+  'copy-failed-ios': '複製失敗。請手動複製網址並在 Safari 開啟頁面',
+  'in-app-hint-android': '請複製下列連結至 Chrome 開啟',
+  'in-app-hint-ios': '請複製下列連結至 Safari 開啟',
   'popup-window-hint': '會在新視窗開啟以完成操作，是否繼續？',
 };
 
