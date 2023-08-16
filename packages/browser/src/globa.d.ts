0e267a908fdc1e32be0c30063bf19db7494bcda4
Steven Fong <waneblade@gmail.com>
2023-08-16 17:48:18 +0800

    [ASL-4516] ts no unchecked indexed access

---
+++ packages/browser/src/globa.d.ts
@@ -0,0 +1,2 @@
+// fully defined in core global.d.ts
+declare module '@qubic-js/eth-json-rpc-infura';
--- packages/browser/src/translation.ts
+++ packages/browser/src/translation.ts
@@ -9,18 +9,27 @@ const enUs = {
   'popup-window-hint': 'Attempt to open Qubic to complete the action. Do you want to proceed ?',
 };
 
-type localeKeys = keyof typeof enUs;
-const zhTw: Record<localeKeys, string> = {
+type TransKey = keyof typeof enUs;
+const zhTw: Record<TransKey, string> = {
   yes: '是',
   no: '否',
   'popup-window-hint': '會在新視窗開啟以完成操作，是否繼續？',
 };
 
-const locales: Record<string, Record<localeKeys, string>> = {
-  'en-us': enUs,
-  'zh-tw': zhTw,
+// this should be written with typescript satisfies but cra doesn't support this feature
+// https://github.com/facebook/create-react-app/issues/12978
+enum LocaleKey {
+  'en-us' = 'en-us',
+  'zh-tw' = 'zh-tw',
+}
+
+const locales: Record<LocaleKey, Record<TransKey, string>> = {
+  [LocaleKey['en-us']]: enUs,
+  [LocaleKey['zh-tw']]: zhTw,
 };
 
-export const t = (key: localeKeys): string => {
-  return language in locales ? locales[language][key] : locales['en-us'][key];
+// type LocaleKey = keyof typeof locales;
+
+export const t = (key: TransKey): string => {
+  return language in locales ? locales[language as LocaleKey][key] : locales['en-us'][key];
 };
