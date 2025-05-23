4f3a5ba04a43b13e97d90972c0c10ee366101dcc
Kevin.Wu <kevin.wu@maicoin.com>
2025-05-23 11:13:52 +0800

    remove REACT_APP_INFURA_NETWORK_KEY from project

--- example/src/wrappedConnectors.ts
+++ example/src/wrappedConnectors.ts
@@ -3,17 +3,12 @@ import { initializeConnector, Web3ReactHooks } from '@web3-react/core';
 import { Connector, Web3ReactStore } from '@web3-react/types';
 import { chainId, autoHideWelcome, enableIframe } from './queryParams';
 
-const { REACT_APP_INFURA_NETWORK_KEY } = process.env as any;
-
-const INFURA_PROJECT_ID = REACT_APP_INFURA_NETWORK_KEY;
-
 const qubic = initializeConnector<QubicWalletConnector>(
   actions =>
     new QubicWalletConnector({
       actions,
       options: {
         chainId,
-        infuraProjectId: INFURA_PROJECT_ID,
         autoHideWelcome,
         enableIframe,
       },
